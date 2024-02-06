01258aa286f855b91be36ad526afe085055897cf
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-02-06 09:41:34 -0500

    Added plugin: nvim-surround.

---
+++ lua/custom/plugins/nvim_surround.lua
@@ -0,0 +1,12 @@
+-- Neovim Surround plugin. This is used to surround text with brackets, quotes, etc.
+
+return {
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end
+}
