cb4a97335fa7ae7c106ccd0d87a3df8437992a0d
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-02-06 09:43:02 -0500

    Added plugin: refactoring.

---
+++ lua/custom/plugins/refactor.lua
@@ -0,0 +1,12 @@
+-- Plugin to refactor code.
+
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter" }
+  },
+  config = function()
+    require("refactoring").setup()
+  end
+}
