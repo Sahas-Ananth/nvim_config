e3331090320c52b7b7e43f8c75d4e75306a81705
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-02-06 09:43:51 -0500

    Added plugin: f-string-toggle.

---
+++ lua/custom/plugins/toggle_f_str.lua
@@ -0,0 +1,12 @@
+-- Toggle F string in python
+
+return {
+  "roobert/f-string-toggle.nvim",
+  ft = "python",
+  config = function()
+    require("f-string-toggle").setup({
+      key_binding = "<leader>tf",
+      key_binding_desc = "Toggle f-string"
+    })
+  end,
+}
