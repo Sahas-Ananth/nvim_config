29b4c0aa70832fd8eba4c42d9720da3b9fe0026c
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-02-06 09:42:37 -0500

    Added plugin: rainbow-delimiters.

---
+++ lua/custom/plugins/rainbow_brackets.lua
@@ -0,0 +1,26 @@
+-- Plugin to have brackets have same color.
+return {
+  "hiphish/rainbow-delimiters.nvim",
+  config = function()
+    local rainbow_delimiters = require("rainbow-delimiters")
+    vim.g.rainbow_delimiters = {
+      strategy = {
+        [''] = rainbow_delimiters.strategy['global'],
+        vim = rainbow_delimiters.strategy['local'],
+      },
+      query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+      },
+      highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+      },
+    }
+  end
+}
