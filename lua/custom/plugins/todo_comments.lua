67efbf4640b9a4cc11189ffcc5f8d0a8de63e0cc
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-02-06 09:43:19 -0500

    Added plugin: todo-comments.

---
+++ lua/custom/plugins/todo_comments.lua
@@ -0,0 +1,12 @@
+-- A plugin that will highlight TODO text in files.
+
+
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
