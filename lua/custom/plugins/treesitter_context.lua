e80c13251561fdb6d98e34cfffed57d710aaf723
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-02-06 09:44:45 -0500

    Added plugin: nvim-treesitter-context.

---
+++ lua/custom/plugins/treesitter_context.lua
@@ -0,0 +1,5 @@
+-- Plugin which shows the current context of the cursor. For example, if you are in a function, it will show the function name. If you are in a class, it will show the class name. If you are in a file, it will show the file name.
+
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+}
