6927b7be5e6f77725ccf6a3542140454b3db2107
Sahasrajit Anantharamakrishnan <anantharamakrishn.sa@northeastern.edu>
2024-02-21 17:44:57 -0500

    Copilot File Formatting fix.
    --> Just Indent/Formatting fix.

--- lua/custom/plugins/github_copilot.lua
+++ lua/custom/plugins/github_copilot.lua
@@ -6,22 +6,22 @@
 
 -- When time permits, try and work with the unofficial plugin for GitHub Copilot which integrates well will nvim-cmp.
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        filetypes = {yaml = true, markdown = true, gitcommit = true, gitrebase = true},
-      })
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup()
-    end
-  }
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        config = function()
+            require('copilot').setup {
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+                filetypes = { yaml = true, markdown = true, gitcommit = true, gitrebase = true },
+            }
+        end,
+    },
+    {
+        'zbirenbaum/copilot-cmp',
+        config = function()
+            require('copilot_cmp').setup()
+        end,
+    },
 }
