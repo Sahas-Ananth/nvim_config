18e62177f222f03b47d6d75b590760d8455c991a
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-11-15 22:30:37 -0500

    Made it modular. Min setup no plugins.

---
+++ lua/keymaps.lua
@@ -0,0 +1,64 @@
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.diagnostic.config {
+    float = {
+        source = 'always',
+        border = 'single',
+    },
+}
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
+
+-- Move selected line / block of text in visual mode.
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { --[[ desc = 'Move selected line / block of text Down in Visual/Visual Line mode.' ]]})
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { --[[ desc = 'Move selected line / block of text Up in Visual/Visual Line mode.']] })
+
+-- Append to end of line in normal mode but keep cursor in place.
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Append next line to end of line in normal mode but keep cursor in place.' })
+
+-- During vertical half page jump keep cursor in the middle of the screen.
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'During vertical half page jump down keep cursor in the middle of the screen.' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'During vertical half page jump up keep cursor in the middle of the screen.' })
+
+-- When searching, keep cursor in the middle of the screen.
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'When searching forwards keep the cursor in the middle.' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'When searching backwards keep the cursor in the middle.' })
+
+-- When pasting over selected text, replace it and do not copy it to the yank register.
+-- vim.keymap.set("x", "<leader>p", [["_dP]]) -- Original from thePrimeagen.
+vim.keymap.set('v', 'p', [["_dP]], { desc = 'Paste over selected text.' })
+vim.keymap.set('n', 'd', [["_d]], { desc = 'Delete to Black Hole register.' })
+
+-- Make current file executable.
+vim.keymap.set('n', '<leader><leader>x', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'Make current file executable.' })
+
+-- Resize with arrows
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', { noremap = true, silent = true, desc = 'Resize: Up.' })
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { noremap = true, silent = true, desc = 'Resize: Down.' })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true, desc = 'Resize: Left.' })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true, desc = 'Resize: Right.' })
+
+-- Keymap to source and run this current lua file
+vim.keymap.set('n', '<leader>fr', '<cmd>w<cr><cmd>source %<cr>', { noremap = true, silent = true, desc = 'Source and run this file.' })
---
+++ lua/options.lua
@@ -0,0 +1,80 @@
+-- Set number and relative number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set highlight on search
+vim.opt.hlsearch = false
+
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.opt.termguicolors = true
+
+-- Don't show the mode, since it's already in the status line
+-- vim.opt.showmode = false
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+-- vim.opt.list = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Number of spaces that Tab in the file counts for.
+vim.opt.tabstop = 4
+
+-- Number of spaces that a <Tab> counts for while performing editing operations, like
+-- inserting a <Tab> or using <BS>.
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.colorcolumn = { 88, 120 }
+
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+
+vim.opt.showtabline = 2 -- always show tabs
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
