8b4563f59b4972f8a018d1b48048c223b9906da5
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2024-11-15 23:59:11 -0500

    Added nvim_cmp and LSP setup without telescope.

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,5 +1,4 @@
-require('lazy').setup({
-}, {
+require('lazy').setup("plugins", {
 --  ui = {
 --    -- If you are using a Nerd Font: set icons to an empty table which will use the
 --    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
---
+++ lua/plugins/nvim_cmp.lua
@@ -0,0 +1,125 @@
+-- Autocompletion plugin
+-- Repo: https://github.com/hrsh7th/nvim-cmp
+
+--- @type LazyPlugin
+return {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        --- @type LazyPlugin
+        {
+            'L3MON4D3/LuaSnip',
+            build = (function()
+                -- Build Step is needed for regex support in snippets.
+                -- This step is not supported in many windows environments.
+                -- Remove the below condition to re-enable on windows.
+                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                    return
+                end
+                return 'make install_jsregexp'
+            end)(),
+            dependencies = {
+                -- Adds a number of user-friendly snippets
+                'rafamadriz/friendly-snippets',
+                config = function()
+                    require('luasnip.loaders.from_vscode').lazy_load()
+                end,
+            },
+            opts = {
+                keep_roots = true,
+                link_roots = true,
+                link_children = true,
+                exit_roots = false,
+                update_events = { 'TextChanged', 'TextChangedI' },
+                enable_autosnippets = true,
+            },
+        },
+        'saadparwaiz1/cmp_luasnip',
+
+        -- Adds LSP completion capabilities
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
+
+        -- Additional Cmp sources
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-cmdline',
+
+        -- LSP Kind Icons (Nicer icons in CMP.)
+        'onsails/lspkind.nvim',
+    },
+    opts = function()
+        local cmp = require 'cmp'
+        local luasnip = require 'luasnip'
+
+        return {
+            view = {
+                entries = {
+                    follow_cursor = true,
+                },
+            },
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            completion = { completeopt = 'menu,menuone,noinsert' },
+            mapping = cmp.mapping.preset.insert {
+                ['<C-n>'] = cmp.mapping.select_next_item(),
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete {},
+                ['<C-CR>'] = cmp.mapping.confirm {
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = false,
+                },
+                ['<C-k>'] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { 'i', 's' }),
+                ['<C-j>'] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { 'i', 's' }),
+            },
+            sources = {
+                -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+                { name = 'lazydev', group_index = 0 },
+                -- { name = 'vimtex' },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'path' },
+                { name = 'buffer' },
+                --{ name = 'copilot' },
+            },
+
+        }
+    end,
+    config = function(_, opts)
+        local cmp = require("cmp")
+        cmp.setup(opts)
+        -- `/` cmdline setup.
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' },
+            },
+        })
+
+        -- `:` cmdline setup.
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'cmdline' },
+            }, {
+                { name = 'path' },
+                { name = 'buffer', keyword_length = 2 },
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false },
+        })
+    end
+}
