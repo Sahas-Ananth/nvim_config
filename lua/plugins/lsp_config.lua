242f5eed97cda56b7119fc4dbcc860fa14dd3b73
Sahasrajit Anantharamakrishnan <sahas_ananth@outlook.com>
2025-02-24 10:07:53 -0500

    Add new LSPs and other tools

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -4,6 +4,7 @@ return {
     'neovim/nvim-lspconfig',
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
     dependencies = {
+        -- 'Sahas-Ananth/blink.cmp',
         -- Automatically install LSPs to stdpath for neovim
         { 'williamboman/mason.nvim', config = true },
         'williamboman/mason-lspconfig.nvim',
@@ -99,6 +100,10 @@ return {
                 --  For example, in C this would take you to the header.
                 map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+                -- Use Cmp for documentation
+                map('K', vim.lsp.buf.hover, 'Hover Documentation')
+                map('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
                 -- The following two autocommands are used to highlight references of the
                 -- word under your cursor when your cursor rests there for a little while.
                 --    See `:help CursorHold` for information about when this is executed
@@ -133,9 +138,11 @@ return {
                 --
                 -- This may be unwanted, since they displace some of your code
                 if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-                    map('<leader>th', function()
+                    map('<leader>Th', function()
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
                     end, '[T]oggle Inlay [H]ints')
+                    -- Turn on inlay_hint
+                    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
                 end
             end,
         })
@@ -155,6 +162,7 @@ return {
         --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+        -- capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
         capabilities.textDocument.foldingRange = {
             dynamicRegistration = false,
             lineFoldingOnly = true,
@@ -171,7 +179,6 @@ return {
         --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
             clangd = {
-                filetypes = { 'h', 'hpp', 'c', 'cpp', 'cuh', 'cu', 'objc', 'objcpp', 'proto' },
                 cmd = {
                     'clangd',
                     '--offset-encoding=utf-16',
@@ -184,30 +191,21 @@ return {
                     '--pch-storage=memory',
                 },
             },
+            dockerls = {},
+            docker_compose_language_service = {},
             basedpyright = {
                 settings = {
                     disableOrganizeImports = true,
                     analysis = {
                         -- ignore = { '*' },
-                        -- useLibraryCodeForTypes = true,
+                        useLibraryCodeForTypes = true,
                         -- typeCheckingMode = 'standard',
                         diagnosticMode = 'workspace',
                         autoImportCompletions = true,
+                        autoSearchPaths = true,
                     },
                 },
             },
-            -- gopls = {},
-            -- pyright = {},
-            -- rust_analyzer = {},
-            -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-            --
-            -- Some languages (like typescript) have entire language plugins that can be useful:
-            --    https://github.com/pmizio/typescript-tools.nvim
-            --
-            -- But for many setups, the LSP (`ts_ls`) will work just fine
-            -- ts_ls = {},
-            --
-
             lua_ls = {
                 -- cmd = {...},
                 -- filetypes = { ...},
@@ -247,15 +245,16 @@ return {
             'clang-format',
             'cmakelint',
             'cmakelang',
+            'codelldb',
             'debugpy',
+            'docformatter',
             'isort',
             'latexindent',
             'markdown-toc',
             'marksman',
             'mdformat',
-            -- 'mypy',
+            'mypy',
             'stylua',
-            'ruff',
         })
         require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
