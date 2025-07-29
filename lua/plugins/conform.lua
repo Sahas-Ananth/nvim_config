b675e4a3ed5da144f0ecee0812fb8ded5afa2dea
Sahas Ananth <sahas_ananth@outlook.com>
2025-07-29 11:52:05 -0400

    Added New UserCommands to Toggle Autoformat

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,31 @@ return {
     -- event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
+    init = function()
+        vim.g.disable_autoformat = false
+        vim.b.disable_autoformat = false
+        vim.api.nvim_create_user_command('FormatDisable', function(args)
+            if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = 'Disable Autoformat',
+            bang = true,
+        })
+        vim.api.nvim_create_user_command('FormatEnable', function(args)
+            if args.bang then
+                vim.b.disable_autoformat = false
+            else
+                vim.g.disable_autoformat = false
+            end
+        end, {
+            desc = 'Enable Autoformat',
+            bang = true,
+        })
+    end,
     keys = {
         {
             -- Customize or remove this keymap to your liking
@@ -32,11 +57,17 @@ return {
             lua = { 'stylua' },
             ['*'] = { 'codespell' },
         },
-        format_on_save = {
-            lsp_fallback = true,
-            async = false,
-            timeout_ms = 500,
-        },
+        format_on_save = function(bufnr)
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            return {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            }
+        end,
         -- Modifying Latex Indent to take search for a local formatting specs. See: https://ctan.uib.no/support/latexindent/documentation/latexindent.pdf#page=98&zoom=264
         formatters = {
             latexindent = {
