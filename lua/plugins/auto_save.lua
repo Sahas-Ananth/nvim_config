036c691e8a22e34629d584c1aa3bed13cd65aeed
Sahas Ananth <sahas_ananth@outlook.com>
2025-07-29 11:53:16 -0400

    Turn off autosave nvim for harpoon

--- lua/plugins/auto_save.lua
+++ lua/plugins/auto_save.lua
@@ -6,5 +6,11 @@ return {
     event = { 'InsertLeave', 'TextChanged' },
     opts = {
         debounce_delay = 10000,
+        condition = function(buf)
+            if vim.bo[buf].filetype == 'harpoon' then
+                return false
+            end
+            return true
+        end,
     },
 }
