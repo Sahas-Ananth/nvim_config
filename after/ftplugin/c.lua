356b603aa1317e6e81e5649f114da27c01b1260a
Sahas Ananth <sahas_ananth@outlook.com>
2025-07-29 11:54:19 -0400

    Just removed a bunch of crap in after/c.lua

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,20 +1,22 @@
-local function setMakeprgm()
-    local cmake_path = vim.fn.getcwd() .. 'CMakeLists.txt'
-    local make_path = vim.fn.getcwd() .. 'Makefile'
-    local cmake_f = io.open(cmake_path, 'r')
-    local make_f = io.open(make_path, 'r')
-    if cmake_f ~= nil then
-        io.close(cmake_f)
-        vim.bo.makeprg = 'cmake -Bbuild . && cmake --build build -j'
-        return
-    end
-    if make_f ~= nil then
-        io.close(make_f)
-        vim.bo.makeprg = 'make'
-        return
-    end
-end
+-- local function setMakeprgm()
+--     local cmake_path = vim.fn.getcwd() .. 'CMakeLists.txt'
+--     local make_path = vim.fn.getcwd() .. 'Makefile'
+--     local cmake_f = io.open(cmake_path, 'r')
+--     local make_f = io.open(make_path, 'r')
+--     if cmake_f ~= nil then
+--         io.close(cmake_f)
+--         vim.bo.makeprg = 'cmake -Bbuild . && cmake --build build -j'
+--         return
+--     end
+--     if make_f ~= nil then
+--         io.close(make_f)
+--         vim.bo.makeprg = 'make'
+--         return
+--     end
+-- end
+--
+-- setMakeprgm()
+-- vim.keymap.set('n', '<leader>cm', '<cmd>Make!<cr>', { noremap = true, buffer = true, silent = true, desc = '[C]ode [M]ake' })
+-- vim.keymap.set('n', '<leader>cC', '<cmd>!rm -rf build<cr><cmd>Make!<cr>', { noremap = true, buffer = true, silent = true, desc = '[C]ode [C]lean Make' })
 
-setMakeprgm()
-vim.keymap.set('n', '<leader>cm', '<cmd>Make!<cr>', { noremap = true, buffer = true, silent = true, desc = '[C]ode [M]ake' })
-vim.keymap.set('n', '<leader>cC', '<cmd>!rm -rf build<cr><cmd>Make!<cr>', { noremap = true, buffer = true, silent = true, desc = '[C]ode [C]lean Make' })
+vim.keymap.set('n', '<leader>cc', '<cmd>ClangdSwitchSourceHeader<cr>', { noremap = false, buffer = true, silent = false, desc = '[C]ode [C]hange to Src/hpp' })
