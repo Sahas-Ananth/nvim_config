41f2b325bf17be3a662ca435cf250f55b58966a3
Sahas Ananth <sahas_ananth@outlook.com>
2025-07-29 11:40:58 -0400

    Make Markdown preview no npm install & no autostart

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -4,11 +4,14 @@ return {
     'iamcco/markdown-preview.nvim',
     ft = { 'markdown' },
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && npm install && git restore .',
+    -- build = 'cd app && npm install && git restore .',
+    build = function()
+        vim.fn['mkdp#util#install']()
+    end,
     init = function()
         local g = vim.g
         g.mkdp_filetypes = { 'markdown' }
-        g.mkdp_auto_start = 1
+        g.mkdp_auto_start = 0
         g.mkdp_auto_close = 0
         g.mkdp_theme = 'light'
     end,
